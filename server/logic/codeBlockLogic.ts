dc367a9850faa6f3424acc48eb5ae84a842ee079
Hila <hiladolev1@gmail.com>
2023-11-18 12:01:25 +0200

    get all code blocks function from the code blocks table

--- server/logic/codeBlockLogic.ts
+++ server/logic/codeBlockLogic.ts
@@ -1,2 +1,9 @@
 import dal_mysql from "../utils/dal_mysql";
 import { OkPacket } from "mysql";
+
+const getAllCodeBlocks = async () => {
+  const SQLcommand = `
+      SELECT * FROM online_coding.code_blocks;
+      `;
+  return await dal_mysql.execute(SQLcommand);
+};
