75d103cab0d4fe2ae5383238c9e9e1318520cdaa
Hila <hiladolev1@gmail.com>
2023-11-18 13:34:55 +0200

    axios api request from server - getting all code blocks

--- client/src/components/pages/Lobby/Lobby.tsx
+++ client/src/components/pages/Lobby/Lobby.tsx
@@ -1,6 +1,23 @@
 import Typography from "@mui/material/Typography";
 import CodeBlockList from "./CodeBlockList/CodeBlockList";
+import axios from "axios";
+import { useEffect } from "react";
+
 function Lobby(): JSX.Element {
+  const fetchCodeBlocks = () => {
+    console.log("getting code blocks from backend....");
+    axios
+      .get(`http://localhost:8080/api/v1/codeBlocks/allCodeBlocks`)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data: ", error);
+      });
+  };
+  useEffect(() => {
+    fetchCodeBlocks();
+  }, []);
   return (
     <div>
       <Typography variant="h1" component="h2">
