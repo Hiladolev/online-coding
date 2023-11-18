76bbffb3f66fe87c497f79cfbd582864f4140f01
Hila <hiladolev1@gmail.com>
2023-11-18 09:45:43 +0200

    added a list component from mui for the code block list

---
+++ client/src/components/pages/Lobby/CodeBlockList.tsx
@@ -0,0 +1,29 @@
+import Box from "@mui/material/Box";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+function CodeBlockList(): JSX.Element {
+  return (
+    <Box sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
+      <nav aria-label="main mailbox folders">
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon></ListItemIcon>
+              <ListItemText primary="Inbox" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon></ListItemIcon>
+              <ListItemText primary="Drafts" />
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </nav>
+    </Box>
+  );
+}
+export default CodeBlockList;
--- client/src/components/pages/Lobby/Lobby.tsx
+++ client/src/components/pages/Lobby/Lobby.tsx
@@ -1,10 +1,12 @@
 import Typography from "@mui/material/Typography";
+import CodeBlockList from "./CodeBlockList";
 function Lobby(): JSX.Element {
   return (
     <div>
       <Typography variant="h1" component="h2">
         Choose Block Code
       </Typography>
+      <CodeBlockList />
     </div>
   );
 }
