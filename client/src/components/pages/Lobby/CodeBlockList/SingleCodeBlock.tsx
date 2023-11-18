c32f6b3d15cc260fe6ae05a524050f644ad66b83
Hila <hiladolev1@gmail.com>
2023-11-18 16:26:37 +0200

    use navigate to navigate into the code block that was clicked(with temporary number)

--- client/src/components/pages/Lobby/CodeBlockList/SingleCodeBlock.tsx
+++ client/src/components/pages/Lobby/CodeBlockList/SingleCodeBlock.tsx
@@ -1,14 +1,16 @@
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemText from "@mui/material/ListItemText";
+import { useNavigate } from "react-router";
 
 interface CodeBlockProps {
   title: string;
 }
 function SingleCodeBlock({ title }: CodeBlockProps): JSX.Element {
+  const navigate = useNavigate();
   return (
     <>
-      <ListItem disablePadding>
+      <ListItem disablePadding onClick={() => navigate("/codeBlock/1")}>
         <ListItemButton>
           <ListItemText inset primary={title} />
         </ListItemButton>
