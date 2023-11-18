0745aca293e08ab92b96c5681120f5cc03c3f7c4
Hila <hiladolev1@gmail.com>
2023-11-18 12:25:32 +0200

    GET method route for getting all code blocks from mysql

--- server/routes/codeBlockRoute.ts
+++ server/routes/codeBlockRoute.ts
@@ -2,3 +2,12 @@ import express, { NextFunction, Request, Response } from "express";
 import logic from "../logic/codeBlockLogic";
 
 const router = express.Router();
+
+router.get(
+  "/allCodeBlocks",
+  async (request: Request, response: Response, next: NextFunction) => {
+    response.status(200).json(await logic.getAllCodeBlocks());
+  }
+);
+
+export default router;
