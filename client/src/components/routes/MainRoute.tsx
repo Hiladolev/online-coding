4ad0d1a61f9d42fdf7d7cccc2c553e9f50313dc7
Hila <hiladolev1@gmail.com>
2023-11-18 14:43:19 +0200

    added code block route with id to know which code block was clicked

--- client/src/components/routes/MainRoute.tsx
+++ client/src/components/routes/MainRoute.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import Lobby from "../pages/Lobby/Lobby";
+import CodeBlock from "../pages/CodeBlock/CodeBlock";
 
 function MainRoute(): JSX.Element {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Lobby />} />
+        <Route path="/codeBlock/:id" element={<CodeBlock />} />
       </Routes>
     </div>
   );
