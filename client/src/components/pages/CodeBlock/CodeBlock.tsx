73a553839bf0a57ee0fa31f3cebcccf64a7ce280
Hila <hiladolev1@gmail.com>
2023-11-18 15:37:09 +0200

    useState for code block object + useEffect for getting the object from the server by it's id + loading p element until the object received

--- client/src/components/pages/CodeBlock/CodeBlock.tsx
+++ client/src/components/pages/CodeBlock/CodeBlock.tsx
@@ -1,12 +1,44 @@
 import { useParams } from "react-router";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import CodeBlockModel from "../../models/CodeBlock";
 
-function CodeBlock(): JSX.Element {
-  const codeBlockId = useParams();
+function CodeBlockPage(): JSX.Element {
+  const params = useParams();
+  const codeBlockId = params.id;
+  const [codeBlockObj, setCodeBlockObj] = useState<
+    CodeBlockModel | undefined
+  >();
+
+  useEffect(() => {
+    const getCodeBlock = () => {
+      axios
+        .get(
+          `http://localhost:8080/api/v1/codeBlocks/codeBlockById/${codeBlockId}`
+        )
+        .then((response) => {
+          setCodeBlockObj(response.data[0]);
+        })
+        .catch((error) => {
+          console.error("Error fetching data: ", error);
+        });
+    };
+
+    getCodeBlock();
+  }, [codeBlockId]);
+
   return (
     <div>
-      <h1>hi</h1>
+      {codeBlockObj ? (
+        <div>
+          <h2>{codeBlockObj.title}</h2>
+          <p>{codeBlockObj.code}</p>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
 }
 
-export default CodeBlock;
+export default CodeBlockPage;
