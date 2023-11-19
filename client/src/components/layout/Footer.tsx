03bfb9eb9253b7d6bb60d90bc4cd3eca6d841e18
Hila <hiladolev1@gmail.com>
2023-11-19 10:01:47 +0200

    footer component - mui template

--- client/src/components/layout/Footer.tsx
+++ client/src/components/layout/Footer.tsx
@@ -1,3 +1,28 @@
+import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import { pink } from "@mui/material/colors";
+
 export default function Footer() {
-  return <div></div>;
+  return (
+    <Box
+      component="footer"
+      sx={{
+        py: 2,
+      }}
+    >
+      <Container maxWidth="sm">
+        <Typography variant="body2" color="text.secondary" align="center">
+          Made with <FavoriteIcon sx={{ color: pink[500] }} /> By &nbsp;
+          <Link color="inherit" href="https://github.com/Hiladolev">
+            Hila Dolev
+          </Link>{" "}
+          {new Date().getFullYear()}
+          {"."}
+        </Typography>
+      </Container>
+    </Box>
+  );
 }
