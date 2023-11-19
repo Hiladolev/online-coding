8156ea8ef530ce19371e4f60d445fa7162f2fd77
Hila <hiladolev1@gmail.com>
2023-11-19 17:08:07 +0200

    removed background image

--- client/src/components/layout/Main.tsx
+++ client/src/components/layout/Main.tsx
@@ -22,11 +22,6 @@ export default function Main() {
         height: "100vh",
         overflow: "hidden",
         flexDirection: "column",
-        backgroundImage:
-          "url('https://cdn.pixabay.com/photo/2017/07/13/16/16/paper-2500942_1280.jpg')",
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        minHeight: "100vh",
         justifyContent: "center",
         alignItems: "center",
         textAlign: "center",
